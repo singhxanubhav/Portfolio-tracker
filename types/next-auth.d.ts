251dc878e48ace2c52bb00b5b2007162c47652a7
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-08-21 14:03:56 +0530

    some changes

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,5 +1,5 @@
 // types/next-auth.d.ts
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
@@ -11,7 +11,7 @@ declare module "next-auth" {
     };
   }
 
-  interface User {
+  interface User extends DefaultUser {
     id: string;
   }
 }
